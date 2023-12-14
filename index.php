8246bf11618e9d6f5e8ccac140aa91e5685e5d6b
MarizMGutib <145555796+MarizMGutib@users.noreply.github.com>
2023-12-14 22:28:36 +0800

    Uploaded index.php for the landing Page

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <title>ShopStock</title>
+        <link rel="website icon" type="png" href="img/logoss.png">
+        <link rel="stylesheet" style="text/css" href="css/newstylesheet.css">
+    </head>
+    </html> 
+    <body>
+    <h1 class="stock">ShopStock</h1>
+        <div class="image"><img src="img/logoss.png" alt=""></div>
+            <div class="logo">
+            <h1></h1>
+            </div>
+            
+        </div>
+        <div class="flip-box">
+  <div class="flip-box-inner">
+    <div class="flip-box-front">
+      <img src="img/pics.jpg" alt="Paris" style="width:100%;height:100%;">
+    </div>
+    <div class="flip-box-back">
+    <div class="ref">
+               <img src="img/cart.png" alt="">
+                <a href="login.php"><button type="submit" name="submit"><strong >Get's Started</strong></button></a>
+            </div>
+      
+    </div>
+  </div>
+</div>
+
+        <div class="h-right_content">
+            <h2>Launch into "ShopStock" Efficiency</h2>
+            <h3 class="hr-lines">Your Business IMS</h3>
+            <p><b></p>
+            <div class="sub-content">
+            
+                <ol>
+                    <li class="h-right_contents">"ShopStock Inventory System: Elevate Your Inventory Management to Unprecedented Heights with our cutting-edge solution. Master inventory excellence—it's a journey of innovation."</li>
+                    
+                </ol>
+            </div>
+        </div>
+       
+</body>
+</html>
